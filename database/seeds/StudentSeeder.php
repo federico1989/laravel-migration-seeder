0ebaae148a5a6c2714958ab9e23e7ce3e2284232
Federico <federico.scarpati1989@gmail.com>
2021-02-22 17:50:21 +0100

    Seeder che prende i dati sotto forma di array

---
+++ database/seeds/StudentSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Student;
+
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $student_list = [
+            [
+            'name'=> 'Gianni',
+            'surname'=> 'Sperti'
+            ],
+            [
+            'name'=> 'Piperita',
+            'surname'=> 'Patty'
+            ],
+            [
+            'name'=> 'Johnny',
+            'surname'=> 'English'
+            ]
+        ];
+
+        foreach ($student_list as $student) {
+           $newStudent = new Student();
+           $newStudent->name = $student['name'];
+           $newStudent->surname = $student['surname'];
+           $newStudent->save();
+        }
+    }
+}
